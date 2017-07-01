e83f77fcc92347ac1f79ab3bf20abef28a2fbd67
fernandorossello <fer89cai@gmail.com>
2017-07-01 20:25:26 -0300

    Modifico layout de página de presentación

--- app/src/main/java/mobile20171c/utnapp/ConfiguracionFragment.java
+++ app/src/main/java/mobile20171c/utnapp/ConfiguracionFragment.java
@@ -1,16 +1,26 @@
+
 package mobile20171c.utnapp;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class ConfiguracionFragment extends Fragment {
+
+    private FirebaseAuth mAuth;
 
     public ConfiguracionFragment() {
         // Required empty public constructor
@@ -27,4 +37,23 @@ public class ConfiguracionFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_configuracion, container, false);
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        EditText etEmail = (EditText)view.findViewById(R.id.editTextEmail);
+        EditText etNombre = (EditText)view.findViewById(R.id.editTextNombre);
+
+        etEmail.setText(user.getEmail());
+        etNombre.setText(user.getDisplayName());
+    }
+    
 }
