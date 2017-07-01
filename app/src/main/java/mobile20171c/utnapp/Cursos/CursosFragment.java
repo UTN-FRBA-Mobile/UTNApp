0793e4e5525659893b77665dabcdd96efb0c4537
Rodrigo Souto <rodrigo@multiplica.com>
2017-07-01 10:32:11 -0300

    Revert "Agrego AsynkTask para obtener la información de Firebase"
    This reverts commit 354272dff4537903732ec6abc9510705b56473b9.

--- app/src/main/java/mobile20171c/utnapp/Cursos/CursosFragment.java
+++ app/src/main/java/mobile20171c/utnapp/Cursos/CursosFragment.java
@@ -1,9 +1,6 @@
 package mobile20171c.utnapp.Cursos;
 
-import android.app.ProgressDialog;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
@@ -18,18 +15,16 @@ import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import mobile20171c.utnapp.R;
 import mobile20171c.utnapp.Recycler.CursosRecyclerAdapter;
 
 
-public class CursosFragment extends Fragment{
+public class CursosFragment extends Fragment {
+
     private CursosRecyclerAdapter misCursosRecycler;
     private CursosRecyclerAdapter todosLosCursosRecycler;
-    private ProgressDialog pr;
-    private RecyclerView recyclerView;
 
     public CursosFragment() {
         // Required empty public constructor
@@ -60,7 +55,21 @@ public class CursosFragment extends Fragment{
             }
         });
 
-        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerViewCursos);
+        final RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recyclerViewCursos);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        misCursosRecycler = new CursosRecyclerAdapter(
+                getContext(),
+                FirebaseDatabase.getInstance().getReference().child("usuarios").child(user.getUid()).child("cursos")
+        );
+
+        todosLosCursosRecycler = new CursosRecyclerAdapter(
+                getContext(),
+                FirebaseDatabase.getInstance().getReference().child("cursos")
+        );
+
+        recyclerView.setAdapter(misCursosRecycler);
 
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
@@ -68,19 +77,17 @@ public class CursosFragment extends Fragment{
         tabs.addTab(tabs.newTab().setText("Mis Cursos"));
         tabs.addTab(tabs.newTab().setText("Todos los cursos"));
 
-        new ObtenerMisCursos().execute();
-
         tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 switch (tab.getPosition()){
                     case 0:
                         // mis cursos
-                        new ObtenerMisCursos().execute();
+                        recyclerView.setAdapter(misCursosRecycler);
                         break;
                     case 1:
                         // todos los cursos
-                        new ObtenerTodosLosCursos().execute();
+                        recyclerView.setAdapter(todosLosCursosRecycler);
                         break;
                 }
             }
@@ -97,68 +104,4 @@ public class CursosFragment extends Fragment{
         });
     }
 
-  private class ObtenerTodosLosCursos extends AsyncTask<Void, Integer, Void> {
-
-
-      @Override
-      protected Void doInBackground(Void... params) {
-          todosLosCursosRecycler = new CursosRecyclerAdapter(
-                  getContext(),
-                  FirebaseDatabase.getInstance().getReference().child("cursos")
-          );
-          return null;
-      }
-
-      @Override
-        protected void onPreExecute() {
-            pr = new ProgressDialog(getContext());
-            pr.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-            pr.setIndeterminate(true);
-            pr.setCancelable(false);
-            pr.setMessage(getResources().getText(R.string.cargando));
-            pr.show();
-            super.onPreExecute();
-        }
-
-        @Override
-        protected void onPostExecute(Void result) {
-
-            recyclerView.setAdapter(todosLosCursosRecycler);
-            pr.dismiss();
-        }
-  }
-
-
-    private class ObtenerMisCursos extends AsyncTask<Void, Integer, Void> {
-
-        @Override
-        protected Void doInBackground(Void... params) {
-            misCursosRecycler = new CursosRecyclerAdapter(
-                    getContext(),
-                    FirebaseDatabase.getInstance().getReference().child("usuarios").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("cursos")
-            );
-            return null;
-        }
-
-        @Override
-        protected void onPreExecute() {
-            pr = new ProgressDialog(getContext());
-            pr.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-            pr.setIndeterminate(true);
-            pr.setCancelable(false);
-            pr.setMessage(getResources().getText(R.string.cargando));
-            pr.show();
-            super.onPreExecute();
-        }
-
-        @Override
-        protected void onPostExecute(Void result) {
-            recyclerView.setAdapter(misCursosRecycler);
-            pr.dismiss();
-        }
-    }
-
 }
-
-
-
