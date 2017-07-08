8970f163c9efc6c3aca8da49ad2ef6e63e0317e7
fernandorossello <fer89cai@gmail.com>
2017-07-08 18:36:05 -0300

    Borro la opción de Settings del menú superior

--- app/src/main/java/mobile20171c/utnapp/MainActivity.java
+++ app/src/main/java/mobile20171c/utnapp/MainActivity.java
@@ -160,21 +160,6 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        switch (id){
-            case R.id.action_settings:
-                    return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     private void desloguearUsuario() {
         mAuth.signOut();
         Intent intent =  new Intent(this,LoginActivity.class);
