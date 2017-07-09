685ac57cc5e42bc32d5384568a6e6f39b1e42244
Rodrigo Souto <rodrigo@multiplica.com>
2017-07-09 19:33:27 -0300

    recibir notificaciones con la app en background y abrir en el mensaje al hacer click

--- app/src/main/java/mobile20171c/utnapp/Firebase/FcmListenerService.java
+++ app/src/main/java/mobile20171c/utnapp/Firebase/FcmListenerService.java
@@ -1,6 +1,11 @@
 package mobile20171c.utnapp.Firebase;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -9,6 +14,7 @@ import com.google.firebase.messaging.RemoteMessage;
 import java.util.Map;
 
 import mobile20171c.utnapp.MainActivity;
+import mobile20171c.utnapp.R;
 
 public class FcmListenerService extends FirebaseMessagingService {
 
@@ -23,7 +29,22 @@ public class FcmListenerService extends FirebaseMessagingService {
 
             Intent intentCurso = new Intent(getApplicationContext(), MainActivity.class);
             intentCurso.putExtra("mensajesCurso", data.get("cursoId").toString());
-            startActivity(intentCurso);
+            intentCurso.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+            PendingIntent resultIntent = PendingIntent.getActivity(this , 0, intentCurso, PendingIntent.FLAG_UPDATE_CURRENT);
+
+            NotificationCompat.Builder mNotificationBuilder = new NotificationCompat.Builder( this)
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentTitle(data.get("title").toString())
+                    .setContentText(data.get("body").toString())
+                    .setAutoCancel(true)
+                    .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                    .setContentIntent(resultIntent);
+
+            NotificationManager notificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+            notificationManager.notify(0, mNotificationBuilder.build());
         }
     }
 
